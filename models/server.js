b1df1d52ff19401840b51169d1fb7ee4ff2f323a
LSuazo1 <luissuazo80@yahoo.com>
2021-09-24 13:24:33 -0600

    Validaciones de jwt

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usuariosPath-"/api/usuarios";
+        this.usuariosPath="/api/usuarios";
         //ruta para jwt
         this.authPath = "/api/auth";
 
